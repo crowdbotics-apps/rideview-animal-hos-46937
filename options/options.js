ed97c4cdb3733af275d661859aaae7de57866e10
Crowdbotics <team@crowdbotics.com>
2024-03-11 20:07:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rideview-animal-hos-46937",
+  url: "https://rideview-animal-hos-46937.botics.co",
+  api: "https://rideview-animal-hos-46937.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
